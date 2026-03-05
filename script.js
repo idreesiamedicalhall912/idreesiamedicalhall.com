aee8930bef2d7fbf761d912d2520c891c750cf43
idreesiamedicalhall912 <idreesiamedicalhall912@gmail.com>
2026-03-05 13:17:36 +0500

    Added New Features Like Add Modal

--- script.js
+++ script.js
@@ -50,3 +50,78 @@ function contactOptions() {
     }
   });
 }
+
+
+
+// Full or less
+
+function showFull() {
+  const productCardCont = document.getElementById("cc-c");
+  productCardCont.classList.add('active');
+}
+function showLess() {
+  const productCardCont = document.getElementById("cc-c");
+  productCardCont.classList.remove('active');
+
+}
+
+let offers = [
+  {
+    id: 'HB1',
+    name: 'Collagen + B-12',
+    img: 'herbiotic_images/1.jpg',
+    disc: '7%',
+
+  },
+  {
+    id: 'HB2',
+    name: 'Biotin Plus + Biotin Shampoo',
+    img: 'herbiotic_images/2.png',
+    disc: '5%',
+
+  },
+]
+
+
+
+// Modal For Offer
+function closeModal(id) {
+  const modal = document.getElementById(id);
+  modal.style.transition = 'all 0.3s ease';
+  modal.style.opacity = '0';
+  modal.style.pointerEvents = 'none';
+}
+
+function modalTimer() {
+  let remaining = 10;
+  const timerDisplay = document.querySelector("#closeTimer");
+  const timer = setInterval(() => {
+    timerDisplay.textContent = "Close in " + remaining + "s";
+    remaining--;
+    if (remaining < 0) {
+      clearInterval(timer);
+      closeModal('offerModal1');
+    }
+  }, 1000);
+}
+
+
+
+function showRandomOffer() {
+  let randomIndex = Math.floor(Math.random() * offers.length);
+  let offer = offers[randomIndex];
+
+  document.getElementById("modalImage").src = offer.img;
+
+  let message = `Hello, I am interested in this offer:
+    Offer ID: ${offer.id}
+    Product: ${offer.name}
+    Discount: ${offer.disc}
+    image: Image: https://idreesiamedicalhall912.github.io/idreesiamedicalhall.com/herbiotic_images/${offer.img}
+  `;
+  document.getElementById("offerLink").href =
+    "https://wa.me/923035591912?text=" + encodeURIComponent(message);
+  modalTimer();
+}
+
+window.onload = showRandomOffer();
